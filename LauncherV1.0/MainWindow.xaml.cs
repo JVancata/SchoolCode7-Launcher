140555c8281f639b4d840d69ec46b48802b83cd4
Jakub Vančata <jakub.vancata@gentlecodes.com>
2017-11-09 12:31:38 +0100

    HELL YEAH

--- LauncherV1.0/MainWindow.xaml.cs
+++ LauncherV1.0/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using FileHelpers;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +19,147 @@ using System.Windows.Shapes;
 
 namespace LauncherV1._0
 {
-    /// <summary>
-    /// Interakční logika pro MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            AllFilesLabel.Content = "";
+            test();
+
         }
+        private List<String> filesPaths = new List<string>();
+        public void test()
+        {
+            //var engine = new FileHelperEngine<Projects>();
+            var dir = new DirectoryInfo(@"C:\Users\vanca\source\repos");
+            FileInfo[] files = dir.GetFiles();
+            var engine = new FileHelperEngine<Projects>();
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            ObservableCollection<Projects> filesCol = new ObservableCollection<Projects>();
+            foreach (var item in dir.GetFiles("*.sln", SearchOption.AllDirectories))
+            {
+
+                string projectPath = item.DirectoryName;
+                var dir2 = new DirectoryInfo(@item.DirectoryName);
+                //Debug.WriteLine(item.DirectoryName);
+                Projects project = new Projects("sad", "asd", "sad", "das");
+                string path = projectPath + "\\data.csv";
+                //Debug.WriteLine(projectPath);
+                if (!System.IO.File.Exists(@path))
+                {
+                    using (System.IO.FileStream fs = System.IO.File.Create(@path))
+                    {
+                        fs.WriteByte(0);
+                    }
+                }
+                foreach (var item2 in dir2.GetFiles("*.exe", SearchOption.AllDirectories))
+                {
+                    if (System.IO.Path.GetFileNameWithoutExtension(item.Name) == System.IO.Path.GetFileNameWithoutExtension(item2.Name))
+                    {
+                        string smtn = Directory.GetParent(item2.DirectoryName).ToString();
+                        string fldr = smtn.Substring(smtn.Length - 3);
+                        if (!(fldr == "obj"))
+                        {
+
+                            string prettyPath = System.IO.Path.GetFileName(System.IO.Path.GetDirectoryName(item2.FullName)).ToString() + "/" + item2.Name.ToString();
+                            string fullName = item2.FullName.ToString();//System.IO.Path.GetFileNameWithoutExtension(item2.Name) + ".sln"
+                            string desc = "none";
+                            try
+                            {
+                                desc = ReadDescription(path);
+                            }
+                            catch
+                            {
+
+                            }
+                            Projects projekt = new Projects(prettyPath, fullName, item2.Name, projectPath, desc);
+                            filesCol.Add(projekt);
+                            List<Projects> listProject = new List<Projects>() { projekt };
+                            engine.WriteFile(projectPath + "\\data.csv", listProject);
+
+                            //filesCol.Add(System.IO.Path.GetFileName(System.IO.Path.GetDirectoryName(item2.FullName)) + "/" + item2.Name);
+                            //filesCol.Add(System.IO.Path.GetFileName(System.IO.Path.GetDirectoryName(item2.FullName)) + "/" + item2.Name);
+                        }
+                    }
+                }
+            }
+            VypisVsech.ItemsSource = filesCol;
+
+            /*var carMake = files
+            .Where(item => item.Extension == ".exe")
+            .Select(item => item);
+            foreach (var item in carMake)
+            {
+            }*/
+        }
+        private string ReadDescription(string path)
+        {
+            var engine = new FileHelperEngine<Projects>();
+
+            // To Read Use:
+            var result = engine.ReadFile(path);
+            Debug.WriteLine(result);
+            /*List<Projects> list = new List<Projects>()
+            {
+                result
+            };*/
+
+            // result is now an array of Customer
+
+            // To Write Use:
+            //engine.WriteFile(path, result);
+
+            return result[0].Description;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Projects projekt = (Projects)VypisVsech.SelectedItems[0];
+            AllFilesLabel.Content = VypisVsech.SelectedItems[0];
+            NameLabel.Content = projekt.Name;
+            Description.Text = projekt.Description;
+            Process.Start(projekt.FullPath);
+            //AllFilesLabel.Content = projekt.FullPath;
+        }
+
+        void PrintText(object sender, SelectionChangedEventArgs args)
+        {
+            Projects projekt = (Projects)VypisVsech.SelectedItems[0];
+            NameLabel.Content = projekt.Name;
+            AllFilesLabel.Content = projekt.FullPath;
+            Debug.WriteLine("Mrdka: "+projekt.Description);
+            //var engine = new FileHelperEngine<Projects>();
+            //var result = engine.ReadFile(projekt.ProjectPath + "\\data.csv");
+            //List<string> descript = ReadDescription(projekt.ProjectPath + "\\data.csv");
+            
+            //Debug.WriteLine(result);
+            Description.Text = projekt.Description;
+            Debug.WriteLine(projekt.ProjectPath);
+            //ListBoxItem lbi = ((sender as ListBox).SelectedItem as ListBoxItem);
+            //tb.Text = "   You selected " + lbi.Content.ToString() + ".";
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string text = Description.Text;
+            try
+            {
+                Projects projekt = (Projects)VypisVsech.SelectedItems[0];
+                projekt.Description = text;
+                var engine = new FileHelperEngine<Projects>();
+
+                List<Projects> listProject = new List<Projects>()
+                {
+                    projekt
+                };
+                engine.WriteFile(projekt.ProjectPath + "\\data.csv", listProject);
+
+            }
+            catch { }
+        }
+        //AllFilesLabel.Content = VypisVsech.SelectedItems[0];
+        //Process.Start(VypisVsech.SelectedItems[0].ToString());
+
     }
 }
---
+++ LauncherV1.0/Projects.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileHelpers;
+
+namespace LauncherV1._0
+{
+    [DelimitedRecord(";")]
+    class Projects
+    {
+        private string prettyName;
+        private string fullPath;
+        private string name;
+        private string projectPath;
+        private string description;
+
+
+        public Projects(string prettyName, string fullPath, string name, string projectPath, string description = "none")
+        {
+            this.prettyName = prettyName;
+            this.fullPath = fullPath;
+            this.name = name;
+            this.projectPath = projectPath;
+            this.description = description;
+        }
+
+        public Projects()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return this.prettyName;
+        }
+
+        public string PrettyName
+        {
+            get
+            {
+                return this.prettyName;
+            }
+            set
+            {
+                this.prettyName = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        public string FullPath
+        {
+            get
+            {
+                return this.fullPath;
+            }
+            set
+            {
+                this.fullPath = value;
+            }
+        }
+
+        public string ProjectPath
+        {
+            get
+            {
+                return this.projectPath;
+            }
+            set
+            {
+                this.projectPath = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return this.description;
+            }
+            set
+            {
+                this.description = value;
+            }
+        }
+    }
+}
